bfc37ede49a89bdc61ae9c37c633ecbe474e1564
Steven Shih <steven0210423@gmail.com>
2022-09-30 16:04:13 +0800

    0930_pic
    新增pic資料夾與兩張截圖
    (9/26的作業)

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 void main(){
+
   var apptitle = Text('我的第一個Flutter app'),
+      // ignore: prefer_const_constructors
       hiFlutter = Text(
-        'Hi, Flutter.',
-        style: TextStyle(fontSize: 50),
+        'Hi, Flutter.\n今天是20220926',
+        style: TextStyle(
+            fontSize: 30,
+            color: Color(0xFF263238),
+            decoration: TextDecoration.underline,
+        ),
       );
 
+  var img = Image.network("https://imgur.com/kyIOhQo.jpg");
   var appBody = Center(
-    child: hiFlutter,
+    child: img,
   );
 
   var appBar = AppBar(
     title: apptitle,
+    backgroundColor: Color(0xFF006064),
   );
 
   var app = MaterialApp(
     home: Scaffold(
       appBar: appBar,
       body: appBody,
+      backgroundColor: Colors.lightBlueAccent,
     ),
   );
 
